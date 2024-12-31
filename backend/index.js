c5196b789a75b7d5836d43fe463a7e23e5812be9
Dujo Lelas <dujo.lelas09@gmail.com>
2024-12-31 17:55:23 +0100

    Docs for Buyer endpoints

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import buyerRouter from "./routes/buyerRoutes.js";
 import chocolateRouter from "./routes/chocolateRoutes.js";
-
 import cors from "cors";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.use("/buyers", buyerRouter);
 app.use("/chocolates", chocolateRouter);
 
@@ -16,6 +18,26 @@ app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to MERN stack tutorial");
 });
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Chocolate Store API",
+      version: "1.0.0",
+      description: "API documentation for Chocolate Store website",
+    },
+    // servers: [
+    //   {
+    //     url: "http://localhost:3000",
+    //   },
+    // ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 mongoose
   .connect(mongoDBURL)
--- backend/routes/buyerRoutes.js
+++ backend/routes/buyerRoutes.js
@@ -2,10 +2,152 @@ import express, { response } from "express";
 const buyerRouter = express.Router();
 import buyerController from "../controllers/buyerController.js";
 
+/**
+ * @swagger
+ *  tags:
+ *    - name: Buyer
+ *      description: The Buyer management API
+ */
+
+/**
+ * @swagger
+ *  /buyers:
+ *    get:
+ *      tags: [Buyer]
+ *      summary: Retrieve a list of all Buyers
+ *      responses:
+ *        200:
+ *          description: Return list of all Buyers
+ */
 buyerRouter.get("/", buyerController.getAllBuyers);
+
+/**
+ * @swagger
+ *  /buyers/{id}:
+ *    get:
+ *      summary: Retrieve a Buyer
+ *      tags: [Buyer]
+ *      parameters:
+ *        - name: id
+ *          description: The Buyer id
+ *          in: path
+ *          required: true
+ *          type: string
+ *      responses:
+ *        200:
+ *          description: Return single Buyer
+ */
 buyerRouter.get("/:id", buyerController.getBuyersById);
+
+/**
+ * @swagger
+ *  /buyers:
+ *    post:
+ *      summary: Create a new Buyer
+ *      tags: [Buyer]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              required:
+ *                - firmName
+ *                - firmAddress
+ *                - description
+ *                - dateEstablished
+ *                - netWorth
+ *                - countriesOfInterest
+ *              properties:
+ *                firmName:
+ *                  type: string
+ *                firmAddress:
+ *                  type: string
+ *                description:
+ *                  type: string
+ *                dateEstablished:
+ *                  type: string
+ *                netWorth:
+ *                  type: number
+ *                countriesOfInterest:
+ *                  type: array
+ *                  items:
+ *                    type: string
+ *              example:
+ *                firmName: Test company
+ *                firmAddress: ABC street, Mexico
+ *                description: Test description
+ *                dateEstablished: 2024-12-31
+ *                netWorth: 0
+ *                countriesOfInterest: ["Mexico"]
+ *      responses:
+ *        200:
+ *          description: Return created Buyer
+ */
 buyerRouter.post("/", buyerController.createBuyer);
+
+/**
+ * @swagger
+ *  /buyers/{id}:
+ *    put:
+ *      summary: Update a Buyer by id
+ *      tags: [Buyer]
+ *      parameters:
+ *        - name: id
+ *          description: The Buyer id
+ *          in: path
+ *          required: true
+ *          type: string
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                firmName:
+ *                  type: string
+ *                firmAddress:
+ *                  type: string
+ *                description:
+ *                  type: string
+ *                dateEstablished:
+ *                  type: string
+ *                netWorth:
+ *                  type: number
+ *                countriesOfInterest:
+ *                  type: array
+ *                  items:
+ *                    type: string
+ *              example:
+ *                firmName: Test company
+ *                firmAddress: ABC street, Mexico
+ *                description: Test description
+ *                dateEstablished: 2024-12-31
+ *                netWorth: 0
+ *                countriesOfInterest: ["Mexico"]
+ *      responses:
+ *        200:
+ *          description: Return updated Buyer
+ */
 buyerRouter.put("/:id", buyerController.updateBuyerById);
+
+/**
+ * @swagger
+ *  /buyers/{id}:
+ *    delete:
+ *      summary: Delete a Buyer by id
+ *      tags: [Buyer]
+ *      parameters:
+ *        - name: id
+ *          description: The Buyer id
+ *          in: path
+ *          required: true
+ *          type: string
+ *      responses:
+ *        200:
+ *          description: The number of deleted Buyers
+ */
 buyerRouter.delete("/:id", buyerController.deleteBuyerById);
 
 export default buyerRouter;
